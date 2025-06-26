d766f380d645150d7637640400b9e4b017d97e83
Saptangshu2006 <saptangshu.dutta@gmail.com>
2025-06-26 10:24:39 +0530

    Added calculator files

---
+++ script.js
@@ -0,0 +1,22 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".buttons button");
+
+let expression = "";
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.textContent;
+
+        if (value === "=") {
+            try {
+                expression = eval(expression).toString();
+            } catch {
+                expression = "Error";
+            }
+        } else if (value === "C") {
+            expression = "";
+        } else {
+            expression += value;
+        }
+        display.value = expression;
+    });
+});
